2a0afa4c55d70ed0af22306ec5c274b7f9901d77
thespacedoctor <nothingbutdave@gmail.com>
2019-07-02 15:52:54 +0100

    x

--- marshallEngine/feeders/data.py
+++ marshallEngine/feeders/data.py
@@ -363,7 +363,7 @@ class data():
             dbConn=self.dbConn
         )
 
-        if not len(rows) and rows[0]["maxId"]:
+        if not len(rows) or rows[0]["maxId"]:
             maxId = 1
         else:
             maxId = rows[0]["maxId"] + 1
