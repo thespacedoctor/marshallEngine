1b74798057b72fa9cfc2d87dd020f856fa43822a
Dave <d.r.young@qub.ac.uk>
2019-08-02 11:02:46 +0100

    updated backup script to remove auto increments

--- backup_marshall_schema_to_here.sh
+++ backup_marshall_schema_to_here.sh
@@ -8,4 +8,16 @@ echo "please give password for the marshall user again"
 /usr/local/mysql/bin/mysqldump -u marshall --password=mar5ha11  marshall meta_workflow_lists_counts webapp_users marshall_fs_column_map >> marshall_schema.sql
 perl -p -i.bak -e "s/DEFINER=\`\w.*?\`@\`.*?\`//g" marshall_schema.sql
 perl -p -i.bak -e "s/ALTER DATABASE .*?CHARACTER.*?;//g" marshall_schema.sql
+perl -p -i.bak -e "s/AUTO_INCREMENT=\d*//g"  marshall_schema.sql
 
+
+echo "want to clear out unit_tests marshall database? [y|n]:"
+
+read moveON
+
+if [[ $moveON != "y" ]] 
+then
+    exit
+fi
+
+mysql -u utuser --password=utpass unit_tests < marshall_schema.sql
