bc87c4366b0edc4327daca6b835d58bf2ccc71f8
Dave <d.r.young@qub.ac.uk>
2020-05-05 12:43:59 +0100

    small fixes

--- README.rst
+++ README.rst
@@ -5,8 +5,8 @@ marshallEngine
     :target: http://marshallEngine.readthedocs.io/en/latest/?badge
     :alt: Documentation Status
 
-.. image:: https://cdn.rawgit.com/thespacedoctor/marshallEngine/master/coverage.svg
-    :target: https://cdn.rawgit.com/thespacedoctor/marshallEngine/master/htmlcov/index.html
+.. image:: https://cdn.jsdelivr.net/gh/thespacedoctor/marshallEngine@master/coverage.svg
+    :target: https://cdn.jsdelivr.net/gh/thespacedoctor/marshallEngine@master/htmlcov/index.html
     :alt: Coverage Status
 
 *A python package and command-line tools for The engine behind the marshall webapp*.
--- backup_marshall_schema_to_here.sh
+++ backup_marshall_schema_to_here.sh
@@ -1,6 +1,6 @@
 # USE THJS SCRIPT TO BACKUP THE MARSHALL SCHEME FROM THE DATABASE CURRENTLY LIVING ON MY MAC
 
-cd ~/git_repos/marshallEngine/marshallEngine/tests/input
+cd ~/git_repos/_packages_/python/marshallEngine/marshallEngine/tests/input
 /usr/local/mysql/bin/mysql -u marshall --password=mar5ha11 marshall -e "update meta_workflow_lists_counts set count = 0"
 echo "please give password for the marshall user"
 /usr/local/mysql/bin/mysqldump -u marshall --password=mar5ha11 --no-data --routines marshall > marshall_schema.sql
@@ -9,7 +9,7 @@ echo "please give password for the marshall user again"
 perl -p -i.bak -e "s/DEFINER=\`\w.*?\`@\`.*?\`//g" marshall_schema.sql
 perl -p -i.bak -e "s/ALTER DATABASE .*?CHARACTER.*?;//g" marshall_schema.sql
 perl -p -i.bak -e "s/AUTO_INCREMENT=\d*//g"  marshall_schema.sql
-
+rm -rf marshall_schema.sql.bak
 
 echo "want to clear out unit_tests marshall database? [y|n]:"
 
