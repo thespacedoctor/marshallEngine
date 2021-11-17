2382ce0df1d7d9329d63a23cbe6e96ba28c854e7
Dave <d.r.young@qub.ac.uk>
2021-11-17 09:09:11 +0000

    fixing atlas and panstarrs links

--- CHANGES.md
+++ CHANGES.md
@@ -2,6 +2,7 @@
 ## Marshall Engine Release Notes  
 
 adding tns-marker into the TNS search requests
+fixed atlas and panstarrs image stamp to download from new URLs
 
 **v1.0.9 - May 7, 2021**  
 
--- marshallEngine/feeders/atlas/data.py
+++ marshallEngine/feeders/atlas/data.py
@@ -186,7 +186,7 @@ class data(basedata):
                 mjdStr = str(int(float(target.split("_")[1])))
                 if target:
                     iid, mjdString, diffId, ippIdet, type = target.split('_')
-                    targetImageURL = "https://star.pst.qub.ac.uk/sne/atlas4/site_media/images/data/atlas4/" % locals() + '/' + \
+                    targetImageURL = "https://star.pst.qub.ac.uk/sne/atlas4/media/images/data/atlas4/" % locals() + '/' + \
                         mjdStr + '/' + target + '.jpeg'
                     objectURL = "https://star.pst.qub.ac.uk/sne/atlas4/candidate/" + iid
 
@@ -194,7 +194,7 @@ class data(basedata):
                 mjdStr = str(int(float(ref.split("_")[1])))
                 if ref:
                     iid, mjdString, diffId, ippIdet, type = ref.split('_')
-                    refImageURL = "https://star.pst.qub.ac.uk/sne/atlas4/site_media/images/data/atlas4/" % locals() + '/' + \
+                    refImageURL = "https://star.pst.qub.ac.uk/sne/atlas4/media/images/data/atlas4/" % locals() + '/' + \
                         mjdStr + '/' + ref + '.jpeg'
                     objectURL = "https://star.pst.qub.ac.uk/sne/atlas4/candidate/" + iid
 
@@ -202,7 +202,7 @@ class data(basedata):
                 mjdStr = str(int(float(diff.split("_")[1])))
                 if diff:
                     iid, mjdString, diffId, ippIdet, type = diff.split('_')
-                    diffImageURL = "https://star.pst.qub.ac.uk/sne/atlas4/site_media/images/data/atlas4/" % locals() + '/' + \
+                    diffImageURL = "https://star.pst.qub.ac.uk/sne/atlas4/media/images/data/atlas4/" % locals() + '/' + \
                         mjdStr + '/' + diff + '.jpeg'
                     objectURL = "https://star.pst.qub.ac.uk/sne/atlas4/candidate/" + iid
 
