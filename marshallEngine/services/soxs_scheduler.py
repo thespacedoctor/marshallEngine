dc5b60cb682de8466829eed36215b5022c7ae731
Dave <d.r.young@qub.ac.uk>
2021-11-18 10:32:48 +0000

    scheduler sync command-line tool added

--- marshallEngine/services/soxs_scheduler.py
+++ marshallEngine/services/soxs_scheduler.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-*request, update and read status of observation blocks with the SOXS Scheduler  *
+*request, update and read status of observation blocks with the SOXS Scheduler*
 
 :Author:
-    David Young
+    Marco Landoni & David Young
 
 :Date Created:
     November 17, 2021
@@ -14,103 +14,148 @@ import sys
 import os
 os.environ['TERM'] = 'vt100'
 from fundamentals import tools
+from fundamentals.mysql import readquery, writequery
 import requests
 import json
-
-# OR YOU CAN REMOVE THE CLASS BELOW AND ADD A WORKER FUNCTION ... SNIPPET TRIGGER BELOW
-# xt-worker-def
 
 
 class soxs_scheduler(object):
     """
-    *The worker class for the soxs_scheduler module*
+    *request, update and read status of observation blocks with the SOXS Scheduler *
 
     **Key Arguments:**
         - ``log`` -- logger
+        - ``dbConn`` -- marshall DB connection
         - ``settings`` -- the settings dictionary
 
     **Usage:**
 
     To setup your logger, settings and database connections, please use the ``fundamentals`` package (`see tutorial here <http://fundamentals.readthedocs.io/en/latest/#tutorial>`_). 
 
-    To initiate a soxs_scheduler object, use the following:
-
-    ```eval_rst
-    .. todo::
-
-        - add usage info
-        - create a sublime snippet for usage
-        - create cl-util for this class
-        - add a tutorial about ``soxs_scheduler`` to documentation
-        - create a blog post about what ``soxs_scheduler`` does
-    ```
+    To initialise the scheduler object, use the following:
 
     ```python
-    usage code 
+    from marshallEngine.services import soxs_scheduler
+    schr = soxs_scheduler(
+        log=log,
+        dbConn=dbConn,
+        settings=settings
+    )
     ```
 
     """
-    # Initialisation
-    # 1. @flagged: what are the unique attrributes for each object? Add them
-    # to __init__
 
     def __init__(
             self,
             log,
+            dbConn,
             settings=False,
 
     ):
         self.log = log
         log.debug("instansiating a new 'soxs_scheduler' object")
         self.settings = settings
-        # xt-self-arg-tmpx
-
-        # 2. @flagged: what are the default attrributes each object could have? Add them to variable attribute set here
-        # Variable Data Atrributes
-
-        # 3. @flagged: what variable attrributes need overriden in any baseclass(es) used
-        # Override Variable Data Atrributes
+        self.dbConn = dbConn
+        self.baseurl = self.settings["scheduler_baseurl"]
 
         # Initial Actions
-
-        self.baseurl = self.settings["scheduler_baseurl"]
+        self.update_scheduler_ob_table()
 
         return None
 
-    # 4. @flagged: what actions does each object have to be able to perform? Add them here
-    # Method Attributes
-    def get(self):
-        """
-        *get the soxs_scheduler object*
+    def request_all_required_auto_obs(
+            self):
+        """*request OBs for all new transients from scheduler*
 
         **Return:**
-            - ``soxs_scheduler``
+            - ``passList, failedIds`` -- 2 lists, one of transientBucketId that have been assigned an OB and the other of those that have failed to be assigned an OB
 
         **Usage:**
 
-        ```eval_rst
-        .. todo::
-
-            - add usage info
-            - create a sublime snippet for usage
-            - create cl-util for this method
-            - update the package tutorial if needed
-        ```
-
         ```python
-        usage code 
+        schr.request_all_required_auto_obs()
         ```
         """
-        self.log.debug('starting the ``get`` method')
+        self.log.debug('starting the ``request_all_required_auto_obs`` method')
 
-        soxs_scheduler = None
+        # GENERATE THE LIST OF TRANSIENTS NEEDING AN OB
+        sqlQuery = f"""
+            SELECT 
+            t.transientBucketId, s.targetName, t.raDeg, t.decDeg, s.latestMag, s.latestMagFilter
+            FROM
+            scheduler_obs s,
+            transientbucketsummaries t
+            WHERE
+            t.transientBucketId = s.transientBucketId AND s.OB_ID is null
+        """
 
-        self.log.debug('completed the ``get`` method')
-        return soxs_scheduler
+        rows = readquery(
+            log=self.log,
+            sqlQuery=sqlQuery,
+            dbConn=self.dbConn)
+        passList = []
+        failedIds = []
 
-    def create_single_auto_ob(
+        for r in rows:
+            # GET FIRST CHARACTER OF FILTER
+            ffilter = r['latestMagFilter']
+            if not ffilter:
+                ffilter = ""
+            else:
+                ffilter = ffilter[0]
+            try:
+                transientBucketId = r['transientBucketId']
+                obid = self._create_single_auto_ob(
+                    transientBucketId=transientBucketId,
+                    target_name=r['targetName'],
+                    raDeg=r['raDeg'],
+                    decDeg=r['decDeg'],
+                    magnitude_list=[
+                        [ffilter, float(r['latestMag'])]],
+                    existenceCheck=False
+                )
+                if -1 == obid:
+                    failedIds.append(transientBucketId)
+                else:
+                    passList.append(transientBucketId)
+            except Exception as e:
+                print(e)
+                failedIds.append(transientBucketId)
+                pass
+
+        print(f"{len(passList)} OBs added to the scheduler, {len(failedIds)} failed to be added.")
+
+        self.log.debug(
+            'completed the ``request_all_required_auto_obs`` method')
+        return (passList, failedIds)
+
+    def update_scheduler_ob_table(
+            self):
+        """*sync the scheduler ob tables with the core marshall tables to bring it up-to-date*
+
+        **Usage:**
+
+        ```python
+        schr.update_scheduler_ob_table()
+        ```
+        """
+        self.log.debug('starting the ``update_scheduler_ob_table`` method')
+
+        sqlQuery = f"""
+            call update_scheduler_obs();
+        """
+        writequery(
+            log=self.log,
+            sqlQuery=sqlQuery,
+            dbConn=self.dbConn
+        )
+
+        self.log.debug('completed the ``update_scheduler_ob_table`` method')
+        return
+
+    def _create_single_auto_ob(
             self,
-            transientBucketID,
+            transientBucketId,
             target_name,
             raDeg,
             decDeg,
@@ -119,7 +164,7 @@ class soxs_scheduler(object):
         """*request to generate a single auto ob from the soxs scheduler*
 
         **Key Arguments:**
-            - ``transientBucketID`` -- the transients ID from the marshall.
+            - ``transientBucketId`` -- the transients ID from the marshall.
             - ``target_name`` -- the master name of the target.
             - ``raDeg`` -- the target RA.
             - ``decDeg`` -- the target declination.
@@ -127,38 +172,25 @@ class soxs_scheduler(object):
             - ``existenceCheck`` -- check local database to see if OB already exists for this transient. Default *True*.
 
         **Return:**
-            - None
-
-        **Usage:**
-
-        ```python
-        usage code 
-        ```
-
-        ---
-
-        ```eval_rst
-        .. todo::
-
-            - add usage info
-            - create a sublime snippet for usage
-            - write a command-line tool for this method
-            - update package tutorial with command-line tool info if needed
-        ```
+            - ``obid`` -- the ID of the OB generated by the scheduler
         """
-        self.log.debug('starting the ``create_single_auto_ob`` method')
+        self.log.debug('starting the ``_create_single_auto_ob`` method')
 
         if existenceCheck:
             # CHECK FOR EXISTENCE OF OB IN LOCAL DATABASE
-            from fundamentals.mysql import readquery
             sqlQuery = f"""
-                
+                select ob_id from scheduler_obs where transientBucketId = {transientBucketId}
             """
             rows = readquery(
                 log=self.log,
                 sqlQuery=sqlQuery,
                 dbConn=self.dbConn
             )
+            if len(rows) and rows[0]['ob_id']:
+                obid = rows[0]['ob_id']
+                self.log.warning(
+                    f'You are trying to create an OB for transient {transientBucketId}, but it is already assigned OBID = {obid}')
+                return obid
 
         try:
             schd_status_code = 0
@@ -172,7 +204,7 @@ class soxs_scheduler(object):
                     "magnitude_list": magnitude_list,
                     "declination": float(decDeg),
                     "target_name": target_name,
-                    "transientBucketID": transientBucketID,
+                    "transientBucketID": transientBucketId,
                     "right_ascension": float(raDeg)
                 })
             )
@@ -192,12 +224,18 @@ class soxs_scheduler(object):
 
         obid = content["payload"][0]['OB_ID']
 
-        self.log.debug('completed the ``create_single_auto_ob`` method')
+        # UPDATE THE SCHEDULER TABLE
+        sqlQuery = f"""
+            update scheduler_obs set ob_id = {obid} where transientBucketId = {transientBucketId};
+        """
+        rows = writequery(
+            log=self.log,
+            sqlQuery=sqlQuery,
+            dbConn=self.dbConn
+        )
+
+        self.log.debug('completed the ``_create_single_auto_ob`` method')
         return obid
 
     # use the tab-trigger below for new method
     # xt-class-method
-
-    # 5. @flagged: what actions of the base class(es) need ammending? ammend them here
-    # Override Method Attributes
-    # method-override-tmpx
--- marshallEngine/services/tests/test_soxs_scheduler.py
+++ marshallEngine/services/tests/test_soxs_scheduler.py
@@ -8,6 +8,9 @@ import yaml
 from marshallEngine.utKit import utKit
 from fundamentals import tools
 from os.path import expanduser
+from docopt import docopt
+from marshallEngine import cl_utils
+doc = cl_utils.__doc__
 home = expanduser("~")
 
 packageDirectory = utKit("").get_project_root()
@@ -47,21 +50,50 @@ if not os.path.exists(pathToOutputDir):
 
 class test_soxs_scheduler(unittest.TestCase):
 
-    def test_soxs_scheduler_function(self):
+    def test_soxs_scheduler_create_single_auto_ob(self):
 
         from marshallEngine.services import soxs_scheduler
         schr = soxs_scheduler(
             log=log,
+            dbConn=dbConn,
             settings=settings
         )
-        obid = schr.create_single_auto_ob(
-            transientBucketID=1,
+        obid = schr._create_single_auto_ob(
+            transientBucketId=1,
             target_name="joe",
             raDeg=23.3445,
             decDeg=-30.034,
             magnitude_list=[["g", 19.06]]
         )
         print(obid)
+
+    def test_soxs_scheduler_request_all_required_auto_obs(self):
+
+        # ADD A ROW TO BE IMPORTED
+        # utKit.refresh_database()
+        from fundamentals.mysql import writequery
+        sqlQuery = """INSERT IGNORE INTO `fs_user_added` (`id`,`candidateID`,`ra_deg`,`dec_deg`,`mag`,`magErr`,`filter`,`observationMJD`,`discDate`,`discMag`,`suggestedType`,`catalogType`,`hostZ`,`targetImageURL`,`objectURL`,`summaryRow`,`ingested`,`htm16ID`,`survey`,`author`,`dateCreated`,`dateLastModified`,`suggestedClassification`,`htm13ID`,`htm10ID`,`transientBucketId`) VALUES (856,'TestSource',155.125958333,-15.1787369444,20.3,NULL,NULL,57627.5,'2016-08-27',20.3,'SN',NULL,0.34,'http://thespacedoctor.co.uk/images/thespacedoctor_icon_white_circle.png','http://thespacedoctor.co.uk',1,0,NULL,'testSurvey','None','2019-07-30 14:25:39','2019-07-30 14:25:39',NULL,NULL,NULL,NULL);""" % locals()
+        writequery(
+            log=log,
+            sqlQuery=sqlQuery,
+            dbConn=dbConn
+        )
+
+        from marshallEngine.feeders.useradded.data import data
+        ingester = data(
+            log=log,
+            settings=settings,
+            dbConn=dbConn
+        ).ingest(withinLastDays=3)
+
+        from marshallEngine.services import soxs_scheduler
+        schr = soxs_scheduler(
+            log=log,
+            dbConn=dbConn,
+            settings=settings
+        )
+        passedOBID, failedOBIDs = schr.request_all_required_auto_obs()
+        print(failedOBIDs)
 
     def test_soxs_scheduler_function_exception(self):
 
