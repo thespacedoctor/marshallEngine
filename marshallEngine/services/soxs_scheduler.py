0838ced0c58dca996f95b2fea6c163663060d965
Marco Landoni <marco.landoni@gmail.com>
2024-09-05 16:51:36 +0200

    x

--- marshallEngine/services/soxs_scheduler.py
+++ marshallEngine/services/soxs_scheduler.py
@@ -300,7 +300,7 @@ class soxs_scheduler(object):
         return None
 
     def remove_classified_obs(self):
-        sqlQuery = 'SELECT t.transientBucketId FROM  pesstoobjects AS t , scheduler_obs AS so  WHERE t.classifiedFlag = 1 AND so.transientBucketId = t.transientBucketId AND so.autoOB = 1'
+        sqlQuery = 'SELECT t.transientBucketId , so.OB_ID FROM  pesstoobjects AS t , scheduler_obs AS so  WHERE t.classifiedFlag = 1 AND so.transientBucketId = t.transientBucketId AND so.autoOB = 1'
         rows = readquery(
                 log=self.log,
                 sqlQuery=sqlQuery,
